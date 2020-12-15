c68d688be28cf7b1298934a017f34a0545202a0a
bruce <bruce_dc@163.com>
2020-12-15 23:34:48 +0800

    导入xlua插件 简单运行lua代码

---
+++ hotfix/Assets/Scripts/Example.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class Example : MonoBehaviour
+{
+    LuaEnv _luaEnv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _luaEnv = new LuaEnv();
+        _luaEnv.DoString("print'Hello XLua!'");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        _luaEnv.Dispose();    
+    }
+}
